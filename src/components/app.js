bbf5c75e2e479d5e50c1d8318068b6e4732585d1
KylePDorsey <kylepdorsey@gmail.com>
2017-06-12 17:14:28 -0500

    Enable data transfer from the api back to the react ap and display content

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Header from './Header';
+import TorchPreview from './TorchPreview';
+import data from '../testData';
 
-const App = () => {
-	return (
+class App extends React.Component {
+	state = {
+		pageHeader: 'The Right Torch',
+		torches: []
+	};
+	componentDidMount() {
+		this.setState({
+			torches: data.torches
+		});
+	}
+	componentWillUnmount() {
+
+	}
+	render() {
+		return (
 		<div className="App">
-			<Header message="Naming Contests" />
+			<Header message={this.state.pageHeader} />
 			<div>
-				...
+				{this.state.torches.map(torch => 
+					<TorchPreview key={torch.id} {...torch} />
+				)}
 			</div>
 		</div>
-	);
+		);
+	}
 };
 
 export default App;
